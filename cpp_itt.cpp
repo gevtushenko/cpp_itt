4faa20b0a9382e72201615b153845aa0b67c5603
Evtushenko Georgy <evtushenko.georgy@gmail.com>
2019-08-25 14:27:58 +0300

    First commit

---
+++ cpp_itt.cpp
@@ -0,0 +1,59 @@
+#include "cpp_itt.h"
+
+namespace cpp_itt
+{
+
+task::task (__itt_domain *d, std::string task_name_arg)
+  : task_name (move (task_name_arg))
+  , p_domain (d)
+{
+  __itt_task_begin (d, __itt_null, __itt_null, __itt_string_handle_create (task_name.c_str ()));
+}
+
+task::~task ()
+{
+  __itt_task_end (p_domain);
+}
+
+domain::domain (std::string domain_name_arg)
+  : domain_name (move (domain_name_arg))
+  , p_impl (__itt_domain_create (domain_name.c_str ()))
+{ }
+
+void domain::disable ()
+{
+  p_impl->flags = 0;
+}
+
+thread::thread (std::string thread_name_arg)
+  : thread_name (move (thread_name_arg))
+{
+  __itt_thread_set_name (thread_name.c_str ());
+}
+
+domain thread::create_domain (std::string domain_name)
+{
+  return domain (move (domain_name));
+}
+
+void thread::ignore()
+{
+  __itt_thread_ignore ();
+}
+
+void pause ()
+{
+  __itt_pause ();
+}
+
+void resume ()
+{
+  __itt_resume ();
+}
+
+thread create_thread_collector (std::string str)
+{
+  return thread (move (str));
+}
+
+}
---
+++ cpp_itt.h
@@ -0,0 +1,78 @@
+//
+// Created by egi on 8/24/19.
+//
+
+#ifndef CPP_ITT_CPP_ITT_H
+#define CPP_ITT_CPP_ITT_H
+
+#include <ittnotify.h>
+#include <string>
+
+#define DISABLE_CPP_ITT \
+  #define INTEL_NO_ITTNOTIFY_API
+
+namespace cpp_itt
+{
+
+class task
+{
+public:
+  ~task ();
+private:
+  task (__itt_domain *d, std::string task_name_arg);
+
+protected:
+  const std::string task_name;
+  __itt_domain * const p_domain = nullptr;
+};
+
+class domain
+{
+public:
+  domain () = delete;
+
+  void disable ();
+
+protected:
+  explicit domain (std::string domain_name_arg);
+
+private:
+  const std::string domain_name;
+  __itt_domain * const p_impl = nullptr;
+
+  friend class thread;
+};
+
+class thread
+{
+public:
+  thread () = default;
+  explicit thread (std::string thread_name_arg);
+
+  static void ignore ();
+
+  /**
+   * Create domain with specified name (URI naming style is recommended)
+   */
+  static domain create_domain (std::string domain_name);
+
+private:
+  const std::string thread_name;
+};
+
+/**
+ * Pause data collecting process for entire application (not only caller thread).
+ * Reduce overhead of collection.
+ */
+static void pause ();
+
+/**
+ * Resume data collection
+ */
+static void resume ();
+
+static thread create_thread_collector () { return {}; }
+static thread create_thread_collector (std::string str);
+}
+
+#endif // CPP_ITT_CPP_ITT_H
